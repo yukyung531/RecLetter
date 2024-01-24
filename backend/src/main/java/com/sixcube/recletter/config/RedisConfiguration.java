4ca2fc62fe391e3bcde7e9231a2278b68c2733f7
전하영 <ycis5013@naver.com>
2024-01-24 17:26:38 +0900

    fix : 레디스에 저장할 value가 객체이므로 setValueSerializer 파라미터 변경

--- backend/src/main/java/com/sixcube/recletter/config/RedisConfiguration.java
+++ backend/src/main/java/com/sixcube/recletter/config/RedisConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
@@ -25,9 +26,11 @@ public class RedisConfiguration {
   public RedisTemplate<String, Object> redisTemplate() {
     RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
     redisTemplate.setKeySerializer(new StringRedisSerializer());
-    redisTemplate.setValueSerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
     redisTemplate.setConnectionFactory(redisConnectionFactory());
 
     return redisTemplate;
   }
+
+
 }
