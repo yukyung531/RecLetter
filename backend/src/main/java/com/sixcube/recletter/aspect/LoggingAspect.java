5e2aacced40ff976623547822dc9ce696e45faf4
이선재 <sj06152@naver.com>
2024-01-24 13:09:35 +0900

    chore: aspect 폴더 이동 및 주석 추가

--- backend/src/main/java/com/sixcube/recletter/aspect/LoggingAspect.java
+++ backend/src/main/java/com/sixcube/recletter/aspect/LoggingAspect.java
@@ -1,4 +1,4 @@
-package com.sixcube.recletter.clip.aspect;
+package com.sixcube.recletter.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -12,21 +12,23 @@ import org.springframework.util.StopWatch;
 @Slf4j
 public class LoggingAspect {
 
+   // 모든 컨트롤러 메서드에 시작, 끝 로그 적용 및 메서드 실행시간 측정
     @Around("execution(* com.sixcube.recletter.*.controller.*.*(..))")
     public Object timeLogAround(ProceedingJoinPoint joinPoint) throws Throwable{
         StopWatch stopWatch=new StopWatch();
 
         String controllerName=joinPoint.getSignature().getDeclaringType().getName().split("[.]")[5];
         String methodName=joinPoint.getSignature().getName();
+
+        //메서드 시작시 로그 > "컨트롤러명.메서드명 : start"
         log.debug(controllerName+"."+methodName+" : start");
         try{
             stopWatch.start();
             return joinPoint.proceed();
         } finally {
             stopWatch.stop();
-
-            log.debug(controllerName+"."+methodName+" : end");
-            log.debug("It took "+stopWatch.getTotalTimeMillis()+"ms");
+            //메서드 종료시 로그 > "컨트롤러명.메서드명 : end(걸린 시간ms)"
+            log.debug(controllerName+"."+methodName+" : end("+stopWatch.getTotalTimeMillis()+"ms)");
         }
 
 
