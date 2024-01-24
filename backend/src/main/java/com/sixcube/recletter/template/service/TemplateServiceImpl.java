31fd027b2b3376f8eaf4df8ee92329a07d061ba8
이선재 <sj06152@naver.com>
2024-01-24 11:14:19 +0900

    feat: Template 리스트 불러오기 기능 구현

--- backend/src/main/java/com/sixcube/recletter/template/service/TemplateServiceImpl.java
+++ backend/src/main/java/com/sixcube/recletter/template/service/TemplateServiceImpl.java
@@ -4,29 +4,42 @@ import com.sixcube.recletter.template.dto.BGM;
 import com.sixcube.recletter.template.dto.Font;
 import com.sixcube.recletter.template.dto.Frame;
 import com.sixcube.recletter.template.dto.Script;
+import com.sixcube.recletter.template.repository.BGMRepository;
+import com.sixcube.recletter.template.repository.FontRepository;
+import com.sixcube.recletter.template.repository.FrameRepository;
+import com.sixcube.recletter.template.repository.ScriptRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class TemplateServiceImpl implements TemplateService{
+@RequiredArgsConstructor
+public class TemplateServiceImpl implements TemplateService {
+
+    private final FrameRepository frameRepository;
+    private final BGMRepository bgmRepository;
+    private final ScriptRepository scriptRepository;
+    private final FontRepository fontRepository;
+
     @Override
     public List<Font> searchFontList() {
-        return null;
+
+        return fontRepository.findAll();
     }
 
     @Override
     public List<Frame> searchFrameList() {
-        return null;
+        return frameRepository.findAll();
     }
 
     @Override
     public List<BGM> searchBGMList() {
-        return null;
+        return bgmRepository.findAll();
     }
 
     @Override
     public List<Script> searchScriptList() {
-        return null;
+        return scriptRepository.findAll();
     }
 }
