fece43a853f72649036b934941f12e8ad15d3071
이선재 <sj06152@naver.com>
2024-01-24 17:14:57 +0900

    feat: 클립 생성, 수정 메서드 예외 생성

--- backend/src/main/java/com/sixcube/recletter/clip/service/ClipServiceImpl.java
+++ backend/src/main/java/com/sixcube/recletter/clip/service/ClipServiceImpl.java
@@ -1,11 +1,18 @@
 package com.sixcube.recletter.clip.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CopyObjectRequest;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.sixcube.recletter.clip.Repository.ClipRepository;
 import com.sixcube.recletter.clip.dto.Clip;
 import com.sixcube.recletter.clip.dto.ClipInfo;
+import com.sixcube.recletter.clip.exception.InvalidClipFormatException;
+import com.sixcube.recletter.clip.exception.SaveClipFailException;
+import com.sixcube.recletter.clip.exception.WeirdClipUserException;
+import com.sixcube.recletter.studio.dto.StudioParticipant;
+import com.sixcube.recletter.studio.dto.StudioParticipantId;
+import com.sixcube.recletter.studio.exception.StudioNotFoundException;
+import com.sixcube.recletter.studio.repository.StudioParticipantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,29 +21,40 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ClipServiceImpl implements ClipService {
 
     private final ClipRepository clipRepository;
+    private final StudioParticipantRepository studioParticipantRepository;
     private final AmazonS3Client amazonS3Client;
-    private final String cloudFront="";
+    private final String cloudFront = "https://d3f9xm3snzk3an.cloudfront.net/";
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
     private final String extension = ".mp4";
 
+    private StudioParticipant checkValidUserClip(Clip clip) {
+        return studioParticipantRepository.findById(new StudioParticipantId(clip.getStudioId(), clip.getClipOwner())).orElseThrow(WeirdClipUserException::new);
+    }
+
     @Override
     public void createClip(Clip clip, MultipartFile file) {
+        if (!file.getContentType().equals("video/mp4")) {
+            throw new InvalidClipFormatException();
+        }
+        checkValidUserClip(clip);
         try {
             clipRepository.save(clip);
             System.out.println(clip);
 
             saveS3Object(clip, file);
-        } catch (IOException e) {
+        } catch (AmazonS3Exception | IOException e) {
             e.printStackTrace();
-
+            clipRepository.delete(clip); //S3에 저장실패했으므로, DB에만 저장된 것 롤백처리
+            throw new SaveClipFailException();
         }
     }
 
@@ -50,9 +68,9 @@ public class ClipServiceImpl implements ClipService {
             amazonS3Client.deleteObject(bucket, prevKey);
 
             clipRepository.save(clip);
-        } catch (IOException e) {
+        } catch (AmazonS3Exception | IOException e) {
             e.printStackTrace();
-
+            throw new SaveClipFailException();
         }
     }
 
@@ -62,9 +80,7 @@ public class ClipServiceImpl implements ClipService {
             String fileKey = getFileKey(clip);
 
             clipRepository.delete(clip);
-            System.out.println("Ready to delete:" + fileKey);
             amazonS3Client.deleteObject(bucket, fileKey);
-            System.out.println("fileDelete OK");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -94,7 +110,7 @@ public class ClipServiceImpl implements ClipService {
         return stringBuilder.toString();
     }
 
-    public void saveS3Object(Clip clip, MultipartFile file) throws IOException {
+    public void saveS3Object(Clip clip, MultipartFile file) throws IOException,AmazonS3Exception {
         String fileKey = getFileKey(clip);
         System.out.println(fileKey);
 
@@ -112,17 +128,17 @@ public class ClipServiceImpl implements ClipService {
 
     @Override
     public List<ClipInfo> searchClipInfoList(String studioId) {
-        List<Clip> clipList=clipRepository.findClipsByStudioId(studioId);
-        List<ClipInfo> clipInfoList=new ArrayList<>();
-        for(Clip clip: clipList){
-            ClipInfo clipInfo=ClipInfo.builder()
+        List<Clip> clipList = clipRepository.findClipsByStudioId(studioId);
+        List<ClipInfo> clipInfoList = new ArrayList<>();
+        for (Clip clip : clipList) {
+            ClipInfo clipInfo = ClipInfo.builder()
                     .clipId(clip.getClipId())
                     .clipTitle(clip.getClipContent())
                     .clipVolume(clip.getClipVolume())
                     .clipOwner(clip.getClipOwner())
                     .clipContent(clip.getClipContent())
                     .clipOrder(clip.getClipOrder())
-                    .clipUrl(cloudFront+getFileKey(clip))
+                    .clipUrl(cloudFront + getFileKey(clip))
                     .build();
             clipInfoList.add(clipInfo);
         }
