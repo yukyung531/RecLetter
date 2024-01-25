18cbdb6706fd4a5206bf5e5893be76eeb2f21750
김연수 <treamor@naver.com>
2024-01-25 13:19:26 +0900

    build: DB자동 생성

--- backend/db/initdb.d/05user.sql
+++ backend/db/initdb.d/05user.sql
@@ -7,4 +7,4 @@ CREATE TABLE IF NOT EXISTS user (
     deleted_at timestamp null default null
 ) ENGINE innoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO S10P12A606.user(user_id, user_email, user_nickname, user_password) VALUES ("ssafy123", "ssafy@ssafy.com", "김싸피","$2a$10$uFOumKhieLIagUaSD8VyHeP9HfmZYLtIMJ9Ha8RKieuJP0L6ivg");
+-- INSERT INTO S10P12A606.user(user_id, user_email, user_nickname, user_password) VALUES ("ssafy123", "ssafy@ssafy.com", "김싸피","$2a$10$uFOumKhieLIagUaSD8VyHeP9HfmZYLtIMJ9Ha8RKieuJP0L6ivg");
