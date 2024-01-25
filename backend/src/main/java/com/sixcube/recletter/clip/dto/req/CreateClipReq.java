f49918a69612a0c90943a622bddcdb9d628d39b4
이선재 <sj06152@naver.com>
2024-01-25 18:07:46 +0900

    proceeding: 예외 처리 중

--- backend/src/main/java/com/sixcube/recletter/clip/dto/req/CreateClipReq.java
+++ backend/src/main/java/com/sixcube/recletter/clip/dto/req/CreateClipReq.java
@@ -1,5 +1,7 @@
 package com.sixcube.recletter.clip.dto.req;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +13,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class CreateClipReq {
 
     private String studioId;
+
+    @NotBlank
+//    @Pattern(regexp = "[^\\/:*?\"<>|]")
     private String clipTitle;
     private String clipContent;
     private MultipartFile clip;
--- backend/src/main/java/com/sixcube/recletter/clip/exception/InvalidClipFormatException.java
+++ backend/src/main/java/com/sixcube/recletter/clip/exception/InvalidClipFormatException.java
@@ -1,4 +1,8 @@
 package com.sixcube.recletter.clip.exception;
 
 public class InvalidClipFormatException extends RuntimeException{
+     public InvalidClipFormatException(){
+        super("저장하려는 파일이 형식에 어긋납니다.");
+    }
+
 }
