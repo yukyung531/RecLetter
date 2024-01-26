5e456b79eedc78a3819db1ec818b6815512c9861
twnkm7089 <twnkm7089@gmail.com>
2024-01-26 10:56:05 +0900

    feat: 영상편집기능 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    optimizeDeps: {
+        exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+    },
     server: {
+        headers: {
+            "Cross-Origin-Opener-Policy": "same-origin",
+            "Cross-Origin-Embedder-Policy": "require-corp",
+        },
         proxy: {
             '/api': {
                 target: 'http://localhost:8080/',
