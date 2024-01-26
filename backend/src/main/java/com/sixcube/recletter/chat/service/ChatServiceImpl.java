21abe9218b86d7daa9daa4873bc3b49b02c3c7ab
권유경 <yukyung531@gmail.com>
2024-01-26 16:27:45 +0900

    refactor: 채팅 참여자 조회 메서드 리팩토링

--- backend/src/main/java/com/sixcube/recletter/chat/service/ChatServiceImpl.java
+++ backend/src/main/java/com/sixcube/recletter/chat/service/ChatServiceImpl.java
@@ -2,8 +2,13 @@ package com.sixcube.recletter.chat.service;
 
 import com.sixcube.recletter.chat.dto.ChatMessage;
 import com.sixcube.recletter.chat.exhandler.ChatException;
+import com.sixcube.recletter.studio.dto.Studio;
+import com.sixcube.recletter.studio.dto.StudioParticipant;
+import com.sixcube.recletter.studio.repository.StudioParticipantRepository;
+import com.sixcube.recletter.studio.repository.StudioRepository;
 import com.sixcube.recletter.user.dto.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +18,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class ChatServiceImpl implements ChatService {
+    @Autowired
+    StudioRepository studioRepository;
 
-    // 각 스튜디오의 참여자 리스트(key - studioId, value - 참여자들)
-    private Map<String, List<String>> chattinglist = new HashMap<>();
+    @Autowired
+    StudioParticipantRepository studioParticipantRepository;
 
     @Override
     public ChatMessage joinChat(String studioId, ChatMessage chatMessage, User user) {
@@ -32,13 +40,6 @@ public class ChatServiceImpl implements ChatService {
 
             // 메시지 sender에 userNickname 등록
             chatMessage.setSender(user.getUserNickname());
-
-            // 채팅방 참여자 리스트에 추가
-            List<String> users = chattinglist.getOrDefault(studioId, new ArrayList<>());
-            if (!users.contains(chatMessage.getSender())) {
-                users.add(chatMessage.getSender());
-            }
-            chattinglist.put(studioId, users);
 
             // 메시지를 보낸 시간 설정
             chatMessage.setTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
@@ -88,12 +89,7 @@ public class ChatServiceImpl implements ChatService {
             chatMessage.setTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
             // 퇴장 메시지 설정
             chatMessage.setContent(chatMessage.getSender() + "님이 퇴장하였습니다.");
-            // 채팅방 참여자 리스트에서 제거
-            List<String> users = chattinglist.get(studioId);
-            if (users != null) {
-                users.remove(chatMessage.getSender());
-                chattinglist.put(studioId, users);
-            }
+
             log.debug("ChatServiceImpl.leaveChat : end");
             return chatMessage;
         } catch(Exception e) {
@@ -102,13 +98,27 @@ public class ChatServiceImpl implements ChatService {
         }
     }
 
+    /**
+     * 현재 채팅방(스튜디오)에 참여하고 있는 유저 리스트 조회
+     * @param studioId
+     * @return
+     */
     @Override
     public List<String> searchChatUserList(String studioId) {
         try {
             log.debug("ChatServiceImpl.searchChatUserList : start");
-            List<String> userList = chattinglist.getOrDefault(studioId, new ArrayList<>());
+            // 스튜디오 아이디에 해당하는 스튜디오 정보 가져오기
+            Studio studio = studioRepository.findById(studioId)
+                    .orElseThrow(() -> new ChatException("Studio not found: " + studioId));
+            // 스튜디오에 참여하고 있는 참여자들 가져오기
+            List<StudioParticipant> participants = studioParticipantRepository.findAllByStudio(studio);
+            // userId 리스트 만들기
+            List<String> users = participants.stream()
+                    .map(participant -> participant.getUser().getUserId())
+                    .collect(Collectors.toList());
             log.debug("ChatServiceImpl.searchChatUserList : end");
-            return userList;
+            // userId 리스트 반환
+            return users;
         } catch(Exception e) {
             log.error("ChatServiceImpl.searchChatUserList : Exception", e);
             throw new ChatException("Error occurred while searching chat user list");
