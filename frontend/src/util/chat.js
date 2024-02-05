b43e42b904ccca1b8cc820dcdff01205a9ef7a03
김연수 <treamor@naver.com>
2024-02-06 01:10:46 +0900

    build: nginx ws proxy 설정

--- frontend/src/util/chat.js
+++ frontend/src/util/chat.js
@@ -3,7 +3,7 @@ import * as StompJs from '@stomp/stompjs';
 import axios from 'axios';
 import { enterChatting } from '../api/chat';
 
-const websocketUrl = 'ws://recletter.me/api/ws';
+const websocketUrl = 'ws://localhost:8081/ws';
 const topic = '/topic';
 const app = '/app/chat';
 let client = null;
