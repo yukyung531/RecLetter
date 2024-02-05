b645be0a1dbcae391ab1682dd994e47c7be12075
김연수 <treamor@naver.com>
2024-02-06 01:37:28 +0900

    build: wss 연결 시도

--- frontend/src/util/chat.js
+++ frontend/src/util/chat.js
@@ -3,7 +3,7 @@ import * as StompJs from '@stomp/stompjs';
 import axios from 'axios';
 import { enterChatting } from '../api/chat';
 
-const websocketUrl = 'ws://recletter.me/ws';
+const websocketUrl = 'wss://recletter.me/ws';
 const topic = '/topic';
 const app = '/app/chat';
 let client = null;
